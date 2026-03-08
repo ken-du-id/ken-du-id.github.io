f4ec941e6bbc68110b12ddf236b7a252aa53d151
dukang <dukang92@gmail.com>
2026-03-08 09:40:32 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://kangdu.github.io'
+  site: 'https://kangdu.github.io',
+  integrations: [sitemap()],
 });
